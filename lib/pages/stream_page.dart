33814296b1c1819580cc4a002e189657d94afa0f
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2024-02-25 11:08:25 -0500

    Stream from future

--- lib/pages/stream_page.dart
+++ lib/pages/stream_page.dart
@@ -1,6 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class StreamPage extends StatelessWidget {
+  CollectionReference productsReference =
+      FirebaseFirestore.instance.collection("products");
+
   Stream<int> counter() async* {
     for (int i = 0; i < 10; i++) {
       yield i;
@@ -11,42 +15,74 @@ class StreamPage extends StatelessWidget {
   }
 
   Future<int> getNumber() async {
-    return 10;
+    return Future.delayed(Duration(seconds: 3), () {
+      return 10;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    //OBTENCIÓN DE VALOR EN FUTURE
-    getNumber().then((value) {
+    print("Init strem");
+    Stream<int> myStream = Stream.fromFuture(getNumber());
+    print("Stream creado");
+    myStream.listen((value) {
+      print("Valor del Stream");
       print(value);
     });
-    //OBTENCIÓN DE VALOR EN STREAM
-    counter().listen(
-      (event) {
-        print(event);
-      },
-    );
+    // //OBTENCIÓN DE VALOR EN FUTURE
+    // getNumber().then((value) {
+    //   print(value);
+    // });
+    // //OBTENCIÓN DE VALOR EN STREAM
+    // counter().listen(
+    //   (event) {
+    //     print(event);
+    //   },
+    // );
     return Scaffold(
       appBar: AppBar(
         title: Text("Stream Page"),
       ),
-      //STREAM BUILDER
+
       body: StreamBuilder(
-        stream: counter(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.hasData) {
-            return Center(
-              child: Text(
-                snapshot.data.toString(),
-                style: TextStyle(fontSize: 40),
-              ),
-            );
-          }
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        },
-      ),
+          stream: FirebaseFirestore.instance.collection("products").snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot doc = snapshot.data!.docs[index];
+                    return Container(
+                      alignment: Alignment.center,
+                      height: 30,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.blueAccent),
+                      child: Text(doc["name"]),
+                    );
+                  });
+            } else {
+              return Text("No data");
+            }
+          }),
+      //STREAM BUILDER
+      // body: StreamBuilder(
+      //   stream: counter(),
+      //   builder: (BuildContext context, AsyncSnapshot snapshot) {
+      //     if (snapshot.hasData) {
+      //       return Center(
+      //         child: Text(
+      //           snapshot.data.toString(),
+      //           style: TextStyle(fontSize: 40),
+      //         ),
+      //       );
+      //     }
+      //     return Center(
+      //       child: CircularProgressIndicator(),
+      //     );
+      //   },
+      // ),
       //FUTURE BUILDER
       // body: FutureBuilder(
       //   future: getNumber(),
